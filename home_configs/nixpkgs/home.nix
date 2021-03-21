b2ef73eb0d58ef7f8ec5132e9b2cbc202af01c38
Andrew Newman <deadloko@gmail.com>
2021-03-21 09:52:17 +0300

    Update zsh to be used in nix-shell

--- home_configs/nixpkgs/home.nix
+++ home_configs/nixpkgs/home.nix
@@ -143,5 +143,23 @@ in
       enable = true;
       theme = "agnoster";
     };
+
+
+    initExtra = ''
+      source $HOME/.config/nixpkgs/dots/agnoster-nix.zsh-theme
+      '';
+
+    plugins = [
+        {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.1.0";
+          sha256 = "0snhch9hfy83d4amkyxx33izvkhbwmindy0zjjk28hih1a9l2jmx";
+        };
+        }
+    ];
   };
 }
